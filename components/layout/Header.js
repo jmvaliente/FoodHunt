0b8128a2440270a4f32a9a794c93b82cf234ad1e
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-05-15 13:15:44 +0200

    Add new styles user panel

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Search from '../ui/Search'
 import Nav from './Nav'
 import Link from 'next/link'
 import styled from '@emotion/styled'
 import { css } from '@emotion/core'
+
+import Button from '../ui/Button'
+
+//Header//
 
 const ContainerHeader = styled.div`
     max-width: 1200px;
@@ -16,34 +20,64 @@ const ContainerHeader = styled.div`
 `
 
 const Logo = styled.p`
-    color: var(--orange);
-    font-size: 4rem;
-    line-height: 0;
-    font-weight: 700;
-    font-family: 'Roboto', serif;
-    margin-right: 2rem;
+color: var(--orange);
+font-size: 4rem;
+line-height: 0;
+font-weight: 700;
+font-family: 'Roboto', serif;
+margin-right: 2rem;
 `
 
+// User Panel//
+
+const UserPanel = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const NameUserPanel = styled.p ` margin-right: 2rem`
+
 const Header = ( ) => {
+
+    const user = true
+
     return(
-        <header
-            css={css`
-                border-bottom: 2px solid var(--grey2);
-                padding: 1rem 0;
-            `}
-        >
+        <header>
             <ContainerHeader>
                 <div>
-                    <Logo>Food Hunt</Logo>
+                    <Link href="/">
+                        <Logo>Food Hunt</Logo>
+                    </Link>
                     <Search/>
                     <Nav/>
                 </div>
-                <div>
-                    <p>Hello: Name</p>
-                    <button type="button">Logout</button>
-                    <Link href="/">Login</Link>
-                    <Link href="/">Create User</Link>
-                </div>
+                <UserPanel>
+                    {user 
+                    
+                    ?(
+                        <Fragment>
+                            <NameUserPanel>Hello: Name</NameUserPanel>
+                            <Button
+                                bgColor="true"
+                            >Logout
+                            </Button>
+                        </Fragment>
+                    )
+                    :
+                    (
+                        <Fragment> 
+                            <Link href="/">
+                                <Button
+                                    bgColor="true"   
+                                >Login</Button>
+                            </Link>
+                            <Link href="/">
+                                <Button>Create User</Button>
+                            </Link>
+                        </Fragment>
+                    )}
+                    
+                </UserPanel>
             </ContainerHeader>
         </header>
     )
